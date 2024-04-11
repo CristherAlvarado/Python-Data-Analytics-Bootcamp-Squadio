6c9afa061e26b4200e479e97bfb2804adcef9249
Cristher <adrianoalrodiguesgd@gmail.com>
2024-04-11 17:13:19 -0300

    Finalizando o curso conhecendo a linguagem

---
+++ Conhecendo-a-linguagem/print_input.py
@@ -0,0 +1,7 @@
+nome = input("Informe o seu nome: ")
+idade = int(input("Informe sua idade: "))
+
+print(nome, idade)
+print(idade, end="...\n")
+print(nome, idade, sep="#", end="...\n")
+print(nome, idade, sep="#")
